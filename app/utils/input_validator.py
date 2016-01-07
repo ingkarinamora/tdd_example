706ae099fdf2cec730df151a9dd8790e0cc98fe7
Kari Mora <ing.karina.mora@gmail.com>
2016-01-07 13:33:39 -0500

    Adds more testing for input validations

--- app/utils/input_validator.py
+++ app/utils/input_validator.py
@@ -1,5 +1,11 @@
 class InputValidator():
     def validate_customer_type(self, customer_type):
-        if customer_type in ['cedula', 'ruc']:
+        return customer_type in ['cedula', 'ruc']
+
+    def validate_cedula(self, cedula):
+        return len(cedula) == 10 and cedula.isdigit() and 0 < int(cedula[:2]) < 25
+
+    def validate_non_empty_letters_string(self, first_name):
+        if first_name:
             return True
         return False
--- test/unit/utils/test_input_validator.py
+++ test/unit/utils/test_input_validator.py
@@ -1,3 +1,5 @@
+from random import randint
+
 from nose.tools import assert_false, assert_true
 
 from tdd_example.app.utils.input_validator import InputValidator
@@ -25,6 +27,82 @@ class TestInputValidator():
     def test_should_return_true_if_type_is_ruc(self):
         # act
         result = self.validator.validate_customer_type('ruc')
+
+        # assert
+        assert_true(result)
+
+    def test_should_return_true_if_type_is_ruc(self):
+        # act
+        result = self.validator.validate_customer_type('ruc')
+
+        # assert
+        assert_true(result)
+
+    def test_should_return_false_if_cedula_has_only_9_digits(self):
+        # act
+        result = self.validator.validate_cedula('123456789')
+
+        # assert
+        assert_false(result)
+
+    def test_should_return_false_if_cedula_has_a_letter(self):
+        # act
+        result = self.validator.validate_cedula('1234E67890')
+
+        # assert
+        assert_false(result)
+
+    def test_should_return_true_if_cedula_has_only_10_digits(self):
+        # act
+        result = self.validator.validate_cedula('1234567890')
+
+        # assert
+        assert_true(result)
+
+    def test_should_return_false_if_first_two_digits_are_greater_than_24(self):
+        # act
+        result = self.validator.validate_cedula('2534567890')
+
+        # assert
+        assert_false(result)
+
+    def test_should_return_false_if_first_two_digits_are_00(self):
+        # act
+        result = self.validator.validate_cedula('0034567890')
+
+        # assert
+        assert_false(result)
+
+    def test_should_return_true_if_first_two_digits_are_between_1_to_24(self):
+
+        for first_digits_number in range(1, 25):
+            #arrange
+            zero = '0' if first_digits_number < 10 else ''
+            cedula = '{0}{1}34567890'.format(zero, first_digits_number)
+
+            # act
+            result = self.validator.validate_cedula(cedula)
+
+            # assert
+            assert_true(result)
+
+    def test_should_return_false_if_first_name_is_empty_string(self):
+        #act
+        result = self.validator.validate_non_empty_letters_string('')
+
+        # assert
+        assert_false(result)
+
+    def test_should_return_false_if_first_name_is_none(self):
+        #act
+        result = self.validator.validate_non_empty_letters_string(None)
+
+        # assert
+        assert_false(result)
+
+    def test_should_return_true_if_string_is_Karina(self):
+        #act
+        result = self.validator.validate_non_empty_letters_string('Karina')
 
         # assert
         assert_true(result)
